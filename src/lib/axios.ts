d48561e0d87f4afb84b8c27443629fef6b4f1235
Leonardo Sarto <leonardosarto1@gmail.com>
2025-05-03 07:37:57 -0300

    Changed ngrok url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { AuthService } from '../services/auth.service';
 
 const api = axios.create({
-    baseURL: 'https://34a5-2804-a44-60de-c100-8823-7a68-2bf1-79c7.ngrok-free.app',
+    baseURL: 'https://pheasant-massive-regularly.ngrok-free.app',
     headers: {
         'Content-Type': 'application/json',
         "ngrok-skip-browser-warning": "69420",

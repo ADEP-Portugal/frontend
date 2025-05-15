ae927e72129fcc7db7edfa332f2a35a182f67451
Leonardo Sarto <leonardosarto1@gmail.com>
2025-05-15 04:12:20 -0300

    Rollback

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
 import { Button } from "../components/ui/button";
-import { CakeIcon, ChartBar, EyeOffIcon, FileText, ListTodo, Loader2Icon, TriangleAlertIcon, Users, UsersIcon } from "lucide-react";
+import { CakeIcon, ChartBar, EyeOffIcon, FileText, LinkIcon, ListTodo, Loader2Icon, TriangleAlertIcon, Users, UsersIcon } from "lucide-react";
 import { Card } from "../components/ui/card";
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
@@ -57,6 +57,11 @@ const Index = () => {
       url: "/associate",
       icon: Users,
     },
+    {
+      title: "Links Úteis",
+      url: "/useful-links",
+      icon: LinkIcon,
+    }
   ];
   const associateService = new AssociateService();
   const eventService = new EventService();
